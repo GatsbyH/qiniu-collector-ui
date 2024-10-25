2be062c275adc044ac95a73f4bb2841b3fa32c4b
GatsbyH <gatsbyh123@gmail.com>
2024-10-25 22:27:08 +0800

    线上环境

--- vite.config.js
+++ vite.config.js
@@ -28,22 +28,22 @@ export default defineConfig(({ mode, command }) => {
       port: 80,
       host: true,
       open: true,
-      proxy: {
-        // https://cn.vitejs.dev/config/#server-proxy
-        '/dev-api': {
-          target: 'http://localhost:8080',
-          changeOrigin: true,
-          rewrite: (p) => p.replace(/^\/dev-api/, '')
-        }
-      }
       // proxy: {
       //   // https://cn.vitejs.dev/config/#server-proxy
-      //   '/prod-api': {
-      //     target: 'http://10.34.34.73:8200',
+      //   '/dev-api': {
+      //     target: 'http://localhost:8080',
       //     changeOrigin: true,
       //     rewrite: (p) => p.replace(/^\/dev-api/, '')
       //   }
       // }
+      proxy: {
+        // https://cn.vitejs.dev/config/#server-proxy
+        '/prod-api': {
+          target: 'http://106.54.234.202:8080',
+          changeOrigin: true,
+          rewrite: (p) => p.replace(/^\/prod-api/, '')
+        }
+      }
     },
     //fix:error:stdin>:7356:1: warning: "@charset" must be the first rule in the file
     css: {
